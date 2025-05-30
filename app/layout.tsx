06947c75121c1f3197b136f84146e0c8ccca39d5
Ihor Makoviichuk <makoviichuk@involve.software>
2025-05-30 13:40:39 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import { Nunito } from 'next/font/google';
+import { Nunito } from "next/font/google";
 
-import './globals.css';
-import { Providers } from '@/shared/components/shared/providers';
+import "./globals.css";
+import { Providers } from "@/shared/components/shared/providers";
 
 const nunito = Nunito({
-  subsets: ['cyrillic'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700', '800', '900'],
+	subsets: ["cyrillic"],
+	variable: "--font-nunito",
+	weight: ["400", "500", "600", "700", "800", "900"],
 });
 
+export const metadata = {
+	title: "NextPizza",
+	icons: {
+		icon: "/logo.png",
+	},
+};
+
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link data-rh='true' rel='icon' href='/logo.png' />
-      </head>
-      <body className={nunito.variable}>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={nunito.variable}>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
