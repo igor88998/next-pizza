84b923b0e5bbc7817e658e47f0a69fbfa2358a8d
Ihor Makoviichuk <makoviichuk@involve.software>
2025-05-30 13:52:14 +0300

    Commented SortPopup

--- shared/components/shared/top-bar.tsx
+++ shared/components/shared/top-bar.tsx
@@ -1,20 +1,25 @@
-import { cn } from '@/shared/lib/utils'
-import React from 'react'
-import { Categories, Container, SortPopup } from '@/shared/components/shared';
-import { Category } from '@prisma/client';
+import { cn } from "@/shared/lib/utils";
+import React from "react";
+import { Categories, Container, SortPopup } from "@/shared/components/shared";
+import { Category } from "@prisma/client";
 
 interface Props {
-    categories: Category[]
-    className?: string
+	categories: Category[];
+	className?: string;
 }
 
 export const TopBar: React.FC<Props> = ({ categories, className }) => {
-    return (
-        <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
-            <Container className='flex items-center justify-between'>
-                <Categories variations={categories}/>
-                <SortPopup />
-            </Container>
-        </div>
-    )
-}
+	return (
+		<div
+			className={cn(
+				"sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10",
+				className
+			)}
+		>
+			<Container className="flex items-center justify-between">
+				<Categories variations={categories} />
+				{/* <SortPopup /> */}
+			</Container>
+		</div>
+	);
+};
